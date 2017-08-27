bc65637dbb01ce8b862c6655237cd07e1d296f65
Sahana Balram <sahana2008balram@gmail.com>
2017-08-26 21:54:28 -0700

    fix code for animate

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,7 +3,20 @@ var movies = ["The Hunger Games", "Star Wars", "Harry Potter", "Twilight"]
 // GIPHY API key and the URL
 
 
-function movieName() {
+function animate() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        alert("Animating");
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        alert("Stilling");
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+}
+
+function renderMovieName() {
     var movieNames = $(this).attr("data-name");
     var giphyURL = 'https://api.giphy.com/v1/gifs/search?api_key=cc9ad884c34f4733900dba633d22a99d&q="' + movieNames + '"&limit=10&offset=0&rating=G&lang=en';
     console.log(giphyURL);
@@ -12,27 +25,35 @@ function movieName() {
         for (var i = 0; i < moviesTriggerButton.length; i++) {
             // creating a new div to store the movie ratings
             var movieDiv = $("<div>");
-            movieDiv.addClass("movies-div");
             // getting rating information from the data
             var movieRatings = moviesTriggerButton[i].rating;
             // storing the rating information for each image in a paragraph
             var movieParagraph = $("<p>").text("Ratings:" + movieRatings);
             // images of the movies will get displayed here
-            var movieImageUrl = giphs.data[i].images.fixed_height_still;           
+            var movieImageUrlStill = giphs.data[i].images.fixed_height_still.url;
+            var movieImageUrlAnimate = giphs.data[i].images.fixed_height.url;
+            console.log(movieImageUrlAnimate);
             // defines jQUERY image object
             var moviesImage = $("<img>");
+            moviesImage.addClass("movies-div");
             // attributes added to the image objactthat is created above
-             moviesImage.attr("src", movieImageUrl.url);
+            moviesImage.attr("src", movieImageUrlStill);
             moviesImage.attr("alt", "movie image");
-            
+            moviesImage.attr("data-state", "still");
+            moviesImage.attr("data-still", movieImageUrlStill);
+            moviesImage.attr("data-animate", movieImageUrlAnimate);
+
             // dump all the images into the image div
             // prepends img object to DOM element with id of images
             movieDiv.prepend(movieParagraph);
             movieDiv.append(moviesImage);
             $("#movies-view").append(movieDiv);
-        }        
+        }
+        $(".movies-div").on("click", animate);
+
     });
 }
+
 function callButtons() {
     $("#movies-button").empty();
     for (var i = 0; i < movies.length; i++) {
@@ -58,6 +79,7 @@ $(document).ready(function () {
         movies.push(newMovies);
         callButtons();
     });
-    $(".movies").on("click", movieName);
+    $(".movies").on("click", renderMovieName);
+
 
 });
